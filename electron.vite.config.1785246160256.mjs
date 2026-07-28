28cd72016221012c18b8330bc13f092a5136c318
sn <843766158@qq.com>
2026-07-28 21:44:46 +0800

    feat(build): 添加 Electron Vite 配置文件
    - 配置主进程构建选项并设置外部依赖
    - 设置预加载脚本构建目录
    - 配置渲染器进程的 Vue 插件和路径别名
    - 添加 SCSS 预处理器配置
    - 定义多进程构建输出目录结构

---
+++ electron.vite.config.1785246160256.mjs
@@ -0,0 +1,43 @@
+// electron.vite.config.ts
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import vue from "@vitejs/plugin-vue";
+var electron_vite_config_default = defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      outDir: "out/main",
+      rollupOptions: {
+        external: ["fluent-ffmpeg", "ffmpeg-static", "ffprobe-static"]
+      }
+    }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      outDir: "out/preload"
+    }
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        "@": resolve("src/renderer/src")
+      }
+    },
+    plugins: [vue()],
+    build: {
+      outDir: "out/renderer"
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern",
+          additionalData: ""
+        }
+      }
+    }
+  }
+});
+export {
+  electron_vite_config_default as default
+};
